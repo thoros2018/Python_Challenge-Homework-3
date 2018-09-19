7ff070941a6162743fea8b3869500b9eba76c90e
thoros2018 <36866749+thoros2018@users.noreply.github.com>
2018-09-18 22:38:16 -0400

    i modified files

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,1 +1,9 @@
-##Empty file
+import csv
+
+with open('Resources/budget_data.csv', 'r') as csv_file:
+    
+    budgetData = csv.reader(csv_file)
+    
+    for line in budgetData:
+        print(line)
+        
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,1 +1,9 @@
-##Empty file
+import csv
+
+with open('Resources\election_data.csv', 'r') as csv_file:
+    
+    elecData = csv.reader(csv_file)
+    
+    for line in elecData:
+        print(line)
+        
