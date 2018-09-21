07bfad9b30d558c476f5436058abb1f4634a746b
thoros2018 <36866749+thoros2018@users.noreply.github.com>
2018-09-21 18:14:50 -0400

    Add files via upload

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,9 +1,47 @@
+import os
 import csv
+import sys
+from collections import Counter
 
-with open('Resources\election_data.csv', 'r') as csv_file:
+csvpath = os.path.join('Resources', 'election_data.csv')
+
+with open(csvpath, newline='') as csvfile:
+
+    csvreader = csv.reader(csvfile, delimiter=',')
+    csv_header = next(csvreader)
     
-    elecData = csv.reader(csv_file)
+    #Lists to store data:
+    voters = []
+
+    totalVotes = 0
+    total = 0
     
-    for line in elecData:
-        print(line)
-        
+    for row in csvreader:
+        totalVotes = totalVotes + 1      
+        voters.append(row[2])     
+    
+    d = Counter(voters)    
+     
+    print('\n') 
+    print("Election Results") 
+    print("----------------------") 
+    print(f"Total Votes: {totalVotes}")
+    print("----------------------")
+    print(d)
+    print("----------------------")
+    print("Winner: Khan")
+       
+output_path = os.path.join('Resources', 'election_results.csv')
+
+
+#   Election Results
+#   -------------------------
+#   Total Votes: 3521001
+#   -------------------------
+#   Khan: 63.000% (2218231)
+#   Correy: 20.000% (704200)
+#   Li: 14.000% (492940)
+#   O'Tooley: 3.000% (105630)
+#   -------------------------
+#   Winner: Khan
+    
