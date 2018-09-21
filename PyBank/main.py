c976bf4f7ad66d831602fa8d52b6e781363aad48
thoros2018 <36866749+thoros2018@users.noreply.github.com>
2018-09-21 18:14:20 -0400

    Add files via upload

--- PyBank/main.py
+++ PyBank/main.py
@@ -9,7 +9,6 @@ with open(csvpath, newline='') as budgetdata:
     csv_header = next(budgetdata)    
 
 #assign lists:
-    TotalMonthList = []
     TotalList1 = []
     TotalList2 = []
     change = []
@@ -21,27 +20,24 @@ with open(csvpath, newline='') as budgetdata:
     for row in csvreader:
         TotalMonths = TotalMonths + 1
         Total = Total + int(row[1]) 
-        
         TotalList1.append(int(row[1]))
         TotalList2.append(row[0])
-        Data1 = TotalList1
-        Data2 = TotalList2
-        
+                
        
     for i in range(len(TotalList1)-1):
         change.append (TotalList1[i+1] - TotalList1[i])
         changemonths.append (TotalList2[i])
 
-    # print(change)
-    # print(changemonths)
     Mean = sum(change) / len(change)
-
+    GreatestIncrease = max(change)
+    GreatestDecrease = min(change)
     
+        
     print(f"Total Months: {TotalMonths}")
     print(f"Total: {Total}")
     print(f"Average Change: {Mean}")
-    # print(f"Greatest Increase: {}")
-    # print(f"Greatest Decrease: {}")
+    print(f"Greatest Increase in Profits: {GreatestIncrease}")
+    print(f"Greatest Decrease in Profits: {GreatestDecrease}")
     
     
     # Total Months: 86
